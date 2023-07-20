1dd63988fdf210d0df3a842302fdbc40b9cd8ed0
Javier <javier.gauna147@gmail.com>
2023-07-20 20:25:15 -0300

    primer configuracion del proyecto

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# BuenSaborAdminFront
+# BuenSaborClienteFront
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.1.4.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'buen-sabor-admin-front'`, () => {
+  it(`should have as title 'buen-sabor-cliente-front'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('buen-sabor-admin-front');
+    expect(app.title).toEqual('buen-sabor-cliente-front');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('buen-sabor-admin-front app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('buen-sabor-cliente-front app is running!');
   });
 });
